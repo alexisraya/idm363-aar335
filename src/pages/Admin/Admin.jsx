cefa09bfe1c6d9966dab5a51b150fde6c836e3c5
alexisraya <aar335@drexel.edu>
2022-12-06 16:06:47 -0500

    :construction: add Admin Product Card
    similar to the Product Card but this one allows the admin to edit the data

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,7 +1,45 @@
+import { collection, onSnapshot, query, getDocs } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { db } from '../../firestore';
+import './Admin.css';
+import AdminProduct from '../../components/AdminProduct/AdminProduct.jsx';
+
 const Admin = () =>{
+    const [products, setProducts] = useState([])
+        useEffect(() => {
+            const getProducts = () => {
+                const productsList = []
+                getDocs(collection(db, "HotSauce")).then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        //productsList.push({...doc.data(),id:doc.id})
+                        //console.log(doc.id, " => ", doc.data());
+                        const productData = {
+                            keyName: doc.id,
+                            ...doc.data(),
+                        }
+                        productsList.push(productData);
+                    })
+                    setProducts(productsList)
+                }).catch((error) => {
+                    console.log(error.message)
+                })
+            }
+            getProducts()
+        }, []
+    )
+    //testing asynch function
+    useEffect(() => {
+        console.log(products);
+    }, [products])
+
     return (
-        <div className="container">
-            <h1>Admin</h1>
+        <div className="pageBody">
+            <div className="productsContainer">
+                {products.map((product) => {
+                    console.log(product)
+                    return <AdminProduct key={product.id} item={product} />
+                })}
+            </div>
         </div>
     )
 }
